18e0291c21ca65b48bf7160fad9b16fe5753e812
nghiamai <nghiamaiquangtri@gmail.com>
2024-06-08 10:34:31 +0700

    done ex 3

--- Start code/Start code/exercise-3/src/App.jsx
+++ Start code/Start code/exercise-3/src/App.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
   /* You will need to use many state to keep the inut values and other needs */
-
+  const [a, setA] = useState();
+  const [b, setB] = useState();
+  const [result, setResult] = useState();
+  const [error, setError] = useState();
   /* You will need some function to handle the key pressed and button events */
+  const onA = (e) => {
+    setA(e.target.value);
+  };
+
+  const onB = (e) => {
+    setB(e.target.value);
+  };
+
+  const check = () => {
+    if (a && b) {
+      setResult(parseInt(a) + parseInt(b));
+    } else {
+      setError("Please enter a number");
+    }
+  };
 
   return (
     <main>
@@ -18,8 +37,8 @@ function App() {
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input value={result || error} disabled />
+      <button onClick={check}>Compute</button>
     </main>
   );
 }
